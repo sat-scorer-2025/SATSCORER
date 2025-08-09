dde45630d07fc577b24b7dd97f7a09c0b5c51931
ansh-dubey7 <anshdubey32432@gmail.com>
2025-08-09 19:38:32 +0530

    changes

--- backend/utils/Socket.js
+++ backend/utils/Socket.js
@@ -6,7 +6,7 @@ let io;
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173', 'http://localhost:5174'],
+      origin: ['http://localhost:5173','http://localhost:5174','https://admin.satscorer.com','https://satscorer.com'],
       credentials: true,
     },
   });
